10d33f7f34544b3b4a2437cdb618edf5eda229e6
atria10 <mario.atria@stevejobs.academy>
2024-05-13 09:54:53 +0200

    finally fixed the wallet connection using wallet adapters

--- components/solana/Balance.tsx
+++ components/solana/Balance.tsx
@@ -1,17 +1,16 @@
 import { useWallet } from "@solana/wallet-adapter-react";
-import { getConnection } from "./functions";
 
 type Props = {};
 
-const Balance = async (props: Props) => {
+const Balance = (props: Props) => {
   const { publicKey } = useWallet();
-
+  console.log(publicKey);
   if (!publicKey) {
     return;
   }
-  const balance = await getConnection().getBalance(publicKey);
+  // const balance = await getConnection().getBalance(publicKey);
 
-  return <div className="badge badge-primary">{balance}</div>;
+  return <div className="badge badge-primary">{publicKey.toBase58()}</div>;
 };
 
 export default Balance;
--- contexts/ContextProvider.tsx
+++ contexts/ContextProvider.tsx
@@ -5,26 +5,30 @@ import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import { WalletModalProvider as ReactUIWalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl } from "@solana/web3.js";
 import { FC, ReactNode, useCallback, useMemo } from "react";
-import { useNetworkConfiguration } from "./NetworkConfigurationProvider";
 
 export const WalletContextProvider: FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const { networkConfiguration } = useNetworkConfiguration();
-  const wallets = useMemo(() => [], []);
-  const network = networkConfiguration as WalletAdapterNetwork;
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   const onError = useCallback((error: WalletError) => {
     console.error(error);
   }, []);
 
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded.
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
+
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} onError={onError} autoConnect={false}>
-        <ReactUIWalletModalProvider>{children}</ReactUIWalletModalProvider>
+        <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   );
